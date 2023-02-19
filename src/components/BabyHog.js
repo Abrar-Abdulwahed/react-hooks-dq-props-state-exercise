25ba30182a947387b79b492c6c5cc1cba1b6dd5b
A-Abd <abrar.abdulwahed@gmail.com>
2023-02-19 22:58:21 +0300

    Completed assignment

--- src/components/BabyHog.js
+++ src/components/BabyHog.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import normalBaby from "../assets/unadulterated-hoglette.png";
 import SunBaby from "../assets/sun-eyes.png";
 import BlueBaby from "../assets/blue-eyes.png";
@@ -8,25 +8,36 @@ import GlowingBaby from "../assets/glowing-eyes.png";
 // perhaps so we could do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
 
 function BabyHog(props) {
+  const {name, hobby, eyeColor} = props;
+  const eyeColorMapper = {
+    'blue': BlueBaby,
+    'sun': SunBaby,
+    'glowing': GlowingBaby
+  }
+  const babyImage = eyeColorMapper[eyeColor] || normalBaby;
+
+  const [weight, setWeight] = useState(200)
   function handleChangeWeight(e) {
     // how can we reuse this for both buttons?
     // perhaps something with e.target.name === "+"
+    /*eslint no-unused-expressions: ["error", { "allowTernary": true }]*/
+    (e.target.name === "-" && weight > 0) ? setWeight(weight - 10) : setWeight(weight + 10);
   }
 
   return (
     <li className="hogbabies">
-      <h1>Name</h1>
-      <h3>Weight:</h3>
-      <h3>Hobby:</h3>
-      <h4>Eye Color:</h4>
+      <h1>Name: {name}</h1>
+      <h3>Weight: {weight}</h3>
+      <h3>Hobby:  {hobby}</h3>
+      <h4>Eye Color: {eyeColor}</h4>
 
-      <button name="+">Increase Weight</button>
-      <button name="-">Decrease Weight</button>
+      <button name="+" onClick={handleChangeWeight}>Increase Weight</button>
+      <button name="-" onClick={handleChangeWeight}>Decrease Weight</button>
 
       <div className="hb-wrap">
         <img
-          src={normalBaby}
-          style={{ height: "200px" }}
+          src={babyImage}
+          style={{ height: weight }}
           alt="MasterBlasterJrJr"
         />
       </div>
--- src/components/HogBoss.js
+++ src/components/HogBoss.js
@@ -1,38 +1,30 @@
 import React, { useState } from "react";
 import Boss from "../assets/boss-hog.png";
 import BabyHog from "./BabyHog";
-// import offspring from "../data.js"
+import offspring from "../data.js"
 
 function HogBoss() {
   const [eyeColor, setEyeColor] = useState("blue");
 
+  const eyeColors = ["Blue", "Sun", "Glowing"]
   function handleChangeEyeColor(e) {
     setEyeColor(e.target.value);
   }
 
   return (
     <div>
-      <input
-        type="radio"
-        name="eyeColor"
-        value="blue"
-        onChange={handleChangeEyeColor}
-      />
-      Blue<br></br>
-      <input
-        type="radio"
-        name="eyeColor"
-        value="sun"
-        onChange={handleChangeEyeColor}
-      />
-      Sun<br></br>
-      <input
-        type="radio"
-        name="eyeColor"
-        value="glowing"
-        onChange={handleChangeEyeColor}
-      />
-      Glowing<br></br>
+      {eyeColors.map((color, index) => {
+        return ([
+          <input
+                key={index}
+                type="radio"
+                name="eyeColor"
+                value={color.toLowerCase()}
+                onChange={handleChangeEyeColor}
+              />,
+              `${color}`, <br></br>
+        ]);
+      })}
       <h2>Name: Blaster Boss</h2>
       <h3>Weight: 2.54 Tons</h3>
       <h3>Eye Color: {eyeColor}</h3>
@@ -40,9 +32,9 @@ function HogBoss() {
         <img id="boss-blaster" src={Boss} alt="" />
       </div>
       <ul className="hoglist">
-        <BabyHog />
-        <BabyHog />
-        <BabyHog />
+        {offspring.map(baby => 
+          <BabyHog key={baby.id} name={baby.name} hobby={baby.hobby} eyeColor={eyeColor}/>
+        )}
       </ul>
     </div>
   );
